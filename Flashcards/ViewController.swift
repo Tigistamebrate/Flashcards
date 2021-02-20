0fd536e48be01b924e3dbf19b13282a84eee8c0f
tam389 <tam389@drexel.edu>
2021-02-20 14:08:01 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Tigist Mebrate on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
