7ad2b78aaa8bbb91d041af692a2bda75e6d8fb22
tam389 <tam389@drexel.edu>
2021-02-21 01:20:37 -0500

    Week 2 Flashcards lab done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerlabel: UILabel!
+    @IBOutlet weak var questionlabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        questionlabel.isHidden=true
+    }
 }
 
